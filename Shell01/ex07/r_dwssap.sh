82a2a18c606c5fce4587a5558c9b51e38c707603
Ali Karabay <akarabay@k1m12s04.42istanbul.com.tr>
2021-10-20 14:55:08 +0300

    Shell01 project missing codes added.

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '/^#/d' | sed -n 'n;p' | cut -d ':' -f1 | rev | sort -r | awk 'NR>= 7 && NR<= 15' | paste -sd ',' - | sed 's/,/, /g' | sed 's/$/./' | tr -d '\n'
+cat /etc/passwd | sed '/^#/d' | sed -n 'n;p' | cut -d ':' -f1 | rev | sort -r | awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | paste -sd ',' - | sed 's/,/, /g' | sed 's/$/./' | tr -d '\n'
